6076f7dc30ccf82f2fcdc5aea127e5c967916436
onur <onurpolatplt@gmail.com>
2021-07-21 23:10:44 +0200

    first creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import "./App.css";
+import { useState } from "react";
+import img from "./calculator.png";
 
 function App() {
+  let newValue = 0;
+  const [counter, setSwitch] = useState(0);
+  // const [counter2, setSwitch2] = useState(0);
+  // // const [newValue, setSwitch3] = useState(0);
+  const increment = () => {
+    newValue = counter + 1;
+
+    return setSwitch(newValue);
+  };
+  const decrement = () => {
+    newValue = counter - 1;
+    return setSwitch(newValue);
+  };
+  const reset = () => {
+    newValue = 0;
+    return setSwitch(newValue);
+  };
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <header>
+        <img src={img} alt="" />
+        <span>React Counter</span>
+      </header>
+      <main>
+        <div>
+          {" "}
+          <button
+            style={{ visibility: counter === 0 ? "hidden" : "visible" }}
+            onClick={decrement}
+          >
+            -
+          </button>
+          <div className="result">
+            <span>{counter}</span>
+          </div>
+          <button
+            style={{ visibility: counter === 10 ? "hidden" : "visible" }}
+            onClick={increment}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button onClick={reset}>Reset</button>
+        </div>
+      </main>
+      <footer>
+        {" "}
+        <p>
+          Made with <span>React</span> at{" "}
+          <a href="https://www.lereacteur.io/">Le Reacteur</a> By{" "}
+          <a href="https://github.com/polatonur">Onur</a>
+        </p>
+      </footer>
     </div>
   );
 }
